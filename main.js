658216d8cfb186c95eebca7e8291d19063afbf79
samuel wangai <swangai7178@gmail.com>
2023-03-23 10:02:33 +0300

    update

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ loop();
 const tl = gsap.timeline({ defaults: { duration: 1 } });
 
 tl.fromTo(mesh.scale, { z: 0, x: 0, y: 0 }, { z: 1, x: 1, y: 1 });
-ti.fromTo("nav", { y: "-100%" }, { y: "0%" });
+tl.fromTo("nav", { y: "-100%" }, { y: "0%" });
 
 tl.fromTo(".title", { opacity: 0 }, { opacity: 1 });
 
